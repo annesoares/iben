138234732ecc5d3fe5b416c291c2a3fc0b65e1bb
Ana <anne.cr.soares@gmail.com>
2025-01-16 01:54:30 -0300

    New card picker logic

--- card-picker.js
+++ card-picker.js
@@ -1,16 +1,19 @@
 function cardPicker(random) {
-    $.getJSON("deck-info.json", function(deck) {
-        $("#card-result-img").attr("src", deck[random].src);
-        $("#card-result-text").innerHTML(deck[random].text);
-    });
+    fetch("deck-info.json")
+        .then(r => r.json())
+        .then(deck => {
+            $("#card-result-img").setAttribute("src", deck[random].src);
+            $("#card-result-text").innerHTML.replace(deck[random].text);
+
+        })
 }
 
-$("#pick-a-card").click(function() {
-    $.ajax({
-        "type": "GET",
-        "url": "http://www.random.org/integers/?num=1&min=0&max=77&col=1&base=10&format=plain&rnd=new",
-        "success": function (data, status, xhr) {
-            cardPicker(parseInt(data));
-        }
-    });  
+$("#pick-a-card").addEventListener("click", function() {
+    request = new Request("http://www.random.org/integers/?num=1&min=0&max=77&col=1&base=10&format=plain&rnd=new")
+    
+    fetch(request)
+        .then(r => r.json())
+        .then(r =>  cardPicker(r) )
+        .catch(err => console.error(err))
+    
 })
